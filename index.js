27574b1b86261b5b818178cd6716090a90f09ac9
Argexitoo <argexitoo@gmail.com>
2021-11-19 16:19:53 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items = [];
+    this.length =0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    return this.items.sort((a,b) => a - b)
+  }
 
-  get(pos) {}
+  get(pos) {
+   if (pos > this.items.length) {
+    throw new Error('OutOfBounds');
+   } else {
+    return this.items[pos]
+   }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+  
 
-  sum() {}
+  sum() {
+   if (this.items.length === 0) {
+     return 0;
+   } else {
+     return this.items.reduce((a,b) => a+b)
+   }
+  }
 
-  avg() {}
+  avg() {
+    let sum = this.sum()
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return sum / this.length;
+    }
+    
+  }
 }
 
 module.exports = SortedList;
